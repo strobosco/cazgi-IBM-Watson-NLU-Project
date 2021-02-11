81523c9411c6c0bd427340c5953e2b08217ce34e
strobosco <niccolonobili@gmail.com>
2021-02-11 20:12:26 +0000

    Server and Client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = new express();
 
 app.use(express.static('client'))
@@ -6,25 +9,121 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+    const { IamAuthenticator } = require("ibm-watson/auth");
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: "2020-08-01",
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
 app.get("/url/emotion", (req,res) => {
+    let NLU = getNLUInstance();
+    let url = req.query.url;
+    const params = {
+        'url': url,
+        'features': {
+            "emotion": {
+                'default': false,
+                'emotion': true
+            }
+        }          
+    };
 
-    return res.send({"happy":"90","sad":"10"});
+    NLU.analyze(params).then(analysisResults => {
+
+        let joy = analysisResults.result.emotion.document.emotion.joy;
+        let sadness = analysisResults.result.emotion.document.emotion.sadness;
+        let fear = analysisResults.result.emotion.document.emotion.fear;
+  
+        return res.send("Text sentiment is: joy: " + joy + " sadness: " + sadness + " fear: " + fear);
+    }).catch(err => {
+        console.log("error" + err);
+    });
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let NLU = getNLUInstance();
+    let url = req.query.url;
+    const params = {
+        'url': url,
+        'features': {
+            "emotion": {
+                'default': false,
+                'sentiment': true
+            }
+        }          
+    };
+
+    NLU.analyze(params).then(analysisResults => {
+        return res.send("This is the sentiment for: " + url);
+    }).catch(err => {
+        console.log("error" + err);
+    });
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let NLU = getNLUInstance();
+    let text = req.query.text;
+    const params = {
+        'text': text,
+        'features': {
+            "emotion": {
+                'default': false,
+                'emotion': true
+            }
+        }          
+    };
+
+    NLU.analyze(params).then(analysisResults => {
+
+        let joy = analysisResults.result.emotion.document.emotion.joy;
+        let sadness = analysisResults.result.emotion.document.emotion.sadness;
+        let fear = analysisResults.result.emotion.document.emotion.fear;
+  
+        return res.send("Text sentiment is: joy: " + joy + " sadness: " + sadness + " fear: " + fear);
+    }).catch(err => {
+        console.log("error" + err);
+    });
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let NLU = getNLUInstance();
+    let text = req.query.text;
+    const params = {
+        'text': text,
+        'features': {
+            "emotion": {
+                'default': false,
+                'sentiment': true
+            }
+        }          
+    };
+
+    NLU.analyze(params).then(analysisResults => {
+
+        let joy = analysisResults.result.emotion.document.emotion.joy;
+        let sadness = analysisResults.result.emotion.document.emotion.sadness;
+        let fear = analysisResults.result.emotion.document.emotion.fear;
+  
+        return res.send("This is the sentiment for: " + text);
+    }).catch(err => {
+        console.log("error" + err);
+    });
 });
 
 let server = app.listen(8080, () => {
